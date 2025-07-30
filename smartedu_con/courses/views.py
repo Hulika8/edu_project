fbaebfebf08b228b627cf6ae8d3c1bae93cd5711
Hülya Basaran <hulikakb@gmail.com>
2025-07-30 19:00:52 +0300

    v

--- smartedu_con/courses/views.py
+++ smartedu_con/courses/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from . models import Category, Course
+from . models import Category, Course, Tag
 
 def course_list(request):
     courses = Course.objects.all().order_by('-date')
     categories = Category.objects.all()
+    tags = Tag.objects.all()
     context = {
         'courses': courses,
-        'categories': categories
+        'categories': categories,
+        'tags': tags
     }
 
     return render(request, 'courses.html', context)
@@ -21,11 +23,24 @@ def course_detail(request, category_slug, course_id):
     #category__slug iki __ yazma sebebi, category alanının slug alanına göre filtreleme yapıyoruz.
     #context ile course_detail.html dosyasına course değişkenini gönderiyoruz.
 
-def category_detail(request, category_slug):
+def category_list(request, category_slug):
     courses = Course.objects.all().filter(category__slug=category_slug)
     categories = Category.objects.all()
+    tags = Tag.objects.all()
     context = {
         'courses': courses,
-        'categories': categories
+        'categories': categories,
+        'tags': tags
+    }
+    return render(request, 'courses.html', context)
+
+def tag_list(request, tag_slug):
+    courses = Course.objects.filter(tags__slug=tag_slug)
+    categories = Category.objects.all()
+    tags = Tag.objects.all()
+    context = {
+        'courses': courses,
+        'categories': categories,
+        'tags': tags
     }
     return render(request, 'courses.html', context)
