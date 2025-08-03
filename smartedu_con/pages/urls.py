dfcce6d4aaf4358b8f22494bf6e5e0fcb7335681
Hülya Basaran <hulikakb@gmail.com>
2025-08-03 17:22:10 +0300

    v

--- smartedu_con/pages/urls.py
+++ smartedu_con/pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . import views
+#from . import views
+from pages.views import IndexView, AboutView
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('about/', views.about, name="about"),
+    path('', IndexView.as_view(), name="index"),
+    path('about/', AboutView.as_view(), name="about"),
     # path(route, views, opt(kısayol ismi)),
 ]
