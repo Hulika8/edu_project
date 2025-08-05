e433fdb33505fe9ea12ed2407fbc668073c2368b
Hülya Basaran <hulikakb@gmail.com>
2025-08-05 20:19:31 +0300

    v

--- smartedu_con/courses/views.py
+++ smartedu_con/courses/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from . models import Category, Course, Tag
 
-def course_list(request):
-    courses = Course.objects.all().order_by('-date')
+def course_list(request, category_slug=None, tag_slug=None):
+    category_page = None
+    tag_page = None
     categories = Category.objects.all()
     tags = Tag.objects.all()
+
+    if category_slug != None:
+        category_page = get_object_or_404(Category, slug=category_slug)
+        courses = Course.objects.filter(available=True, category=category_page)
+
+    elif tag_slug != None:
+        tag_page = get_object_or_404(Tag, slug=tag_slug)
+        courses = Course.objects.filter(available=True, tags=tag_page)
+
+    else:
+        courses = Course.objects.all().order_by('-date')
+
     context = {
         'courses': courses,
         'categories': categories,
@@ -12,6 +25,7 @@ def course_list(request):
     }
 
     return render(request, 'courses.html', context)
+
 
 def course_detail(request, category_slug, course_id):
     course = Course.objects.get(category__slug=category_slug, id=course_id)
@@ -23,10 +37,11 @@ def course_detail(request, category_slug, course_id):
     #category__slug iki __ yazma sebebi, category alanının slug alanına göre filtreleme yapıyoruz.
     #context ile course_detail.html dosyasına course değişkenini gönderiyoruz.
 
-def category_list(request, category_slug):
-    courses = Course.objects.all().filter(category__slug=category_slug)
+def search(request):
+    courses = Course.objects.filter(name__contains=request.GET['search'])
     categories = Category.objects.all()
     tags = Tag.objects.all()
+    
     context = {
         'courses': courses,
         'categories': categories,
@@ -34,13 +49,40 @@ def category_list(request, category_slug):
     }
     return render(request, 'courses.html', context)
 
-def tag_list(request, tag_slug):
-    courses = Course.objects.filter(tags__slug=tag_slug)
-    categories = Category.objects.all()
-    tags = Tag.objects.all()
-    context = {
-        'courses': courses,
-        'categories': categories,
-        'tags': tags
-    }
-    return render(request, 'courses.html', context)
+
+
+# def course_list(request):
+#     courses = Course.objects.all().order_by('-date')
+#     categories = Category.objects.all()
+#     tags = Tag.objects.all()
+#     context = {
+#         'courses': courses,
+#         'categories': categories,
+#         'tags': tags
+#     }
+
+#     return render(request, 'courses.html', context)
+
+
+
+# def category_list(request, category_slug):
+#     courses = Course.objects.all().filter(category__slug=category_slug)
+#     categories = Category.objects.all()
+#     tags = Tag.objects.all()
+#     context = {
+#         'courses': courses,
+#         'categories': categories,
+#         'tags': tags
+#     }
+#     return render(request, 'courses.html', context)
+
+# def tag_list(request, tag_slug):
+#     courses = Course.objects.filter(tags__slug=tag_slug)
+#     categories = Category.objects.all()
+#     tags = Tag.objects.all()
+#     context = {
+#         'courses': courses,
+#         'categories': categories,
+#         'tags': tags
+#     }
+#     return render(request, 'courses.html', context)
