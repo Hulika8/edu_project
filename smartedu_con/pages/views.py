d318edc4164bd3550af131199dd2fdf10a576221
Hülya Basaran <hulikakb@gmail.com>
2025-08-06 19:28:02 +0300

    v

--- smartedu_con/pages/views.py
+++ smartedu_con/pages/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic.edit import FormView
 from courses.models import Course
+from .forms import ContactForm
+from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
 
 # Create your views here
 # def index(request):
@@ -18,3 +22,13 @@ class AboutView(TemplateView):
     template_name = 'about.html'
 # def about(request):
 #     return render(request, 'about.html')
+
+class ContactView(SuccessMessageMixin, FormView):
+    template_name = 'contact.html'
+    form_class = ContactForm
+    success_url = reverse_lazy('contact')
+    success_message = "Your message has been sent successfully"
+    
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
