cbf6b5662ec38c134d7dbd1e8ab9d475a0794718
Hülya Basaran <hulikakb@gmail.com>
2025-08-09 18:13:54 +0300

    v

--- smartedu_con/accounts/views.py
+++ smartedu_con/accounts/views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from . forms import LoginForm, RegisterForm
 
 # Create your views here.
 
 def user_login(request):
-    return render(request, 'login.html')
+    if request == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('index')
+
+                else:
+                    messages.info(request, 'Your account is not active')
+
+            else:
+                messages.info(request, 'Check your username and password')
+
+    else:
+        form = LoginForm()
+        
+    return render(request, 'login.html', {'form': form})
 
 def user_register(request):
     return render(request, 'register.html')
